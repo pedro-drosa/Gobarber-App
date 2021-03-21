92fffe3f777d2f588c387fa9194a8d49c32fc0c0
pedro-drosa <pedro_drosa@hotmail.com>
2021-03-21 15:11:53 -0300

    private routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+
+import { useAuth } from '../../hooks/Auth';
+
+const Dashboard: React.FC = () => {
+  const {signOut} = useAuth();
+  return (
+    <View>
+      <Button title="sair" onPress={signOut} />
+    </View>
+  )
+}
+
+export default Dashboard;
